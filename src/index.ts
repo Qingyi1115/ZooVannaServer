6ad18d0d5292a57a7e3fec512ef5ab8c5d164147
Qingyi <xqy1115@gmail.com>
2023-08-29 21:50:39 +0800

    Connected to MySQL

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,13 @@
-import express from 'express';
+require("dotenv").config();
+
+import express, {Express, Request, Response}  from 'express';
 import http from 'http';
 import bodyParser from 'body-parser';
 import cookieParser from 'cookie-parser';
 import compression from 'compression';
 import cors from 'cors';
+import mysql from "mysql2";
+import 'dotenv/config';
 
 // import router from './router';
 
@@ -17,11 +21,39 @@ app.use(compression());
 app.use(cookieParser());
 app.use(bodyParser.json());
 
+const db = mysql.createConnection({
+  // host: 'localhost',
+  // user:'root',
+  // password: '',
+    host: process.env.MYSQL_HOST,
+    user: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+    database: process.env.MYSQL_DB,
+    // port:3306,
+    // database: "employees",
+    // port:3306,
+    // multipleStatements: true
+})
+
 const server = http.createServer(app);
 
-server.listen(8080, () => {
-  console.log('Server running on http://localhost:8080/');
+const port = 3000;
+
+server.listen(port, () => {
+  console.log(`Server running on http://localhost:${port}/`);
+});
+
+app.get("/", (req: Request, res: Response) => {
+    res.send("HELLO)s");
 });
 
 
-// app.use('/', router());
+// app.get('/createdb', (req, res) => {
+//   let sql = 'CREATE DATABASE test002'
+//   db.query(sql, err => {
+//     if(err) {
+//       throw err;
+//     }
+//     res.send("Database Created");
+//   })
+// });
