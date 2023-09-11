9691b80738826ce97003c79e1ed092e5c42b9bf0
Owner <e0540022@u.nus.edu>
2023-09-11 11:01:36 +0800

    Added update facilityDetail

--- src/controllers/assetFacilityController.ts
+++ src/controllers/assetFacilityController.ts
@@ -2,6 +2,7 @@ import { Request, Response } from "express";
 import { findEmployeeByEmail } from "../services/user";
 import { PlannerType } from "../models/enumerated";
 import { createNewFacility, getFacilityById } from "../services/assetFacility";
+import { Facility } from "models/facility";
 
 export async function createFacility(req: Request, res: Response) {
     
@@ -38,14 +39,13 @@ export async function updateFacility(req: Request, res: Response) {
             return res.status(403).json({error: "Access Denied! Operation managers only!"});
         }
 
-        const { facilityId, facilityName, xCoordinate, yCoordinate } = req.body
+        const { facilityId, facilityName, xCoordinate, yCoordinate , facilityDetailJson} = req.body
 
-        if (!facilityId || [facilityName, xCoordinate, yCoordinate].every(field => field === undefined)){
+        if (!facilityId || [facilityName, xCoordinate, yCoordinate, facilityDetailJson].every(field => field === undefined)){
             return res.status(400).json({error:"Missing information!"})
         }
 
         let facility = await getFacilityById(Number(facilityId)) as any; 
-        console.log(facility);
         
         if (!facility) return res.status(400).json({ error: "Unknown facilityId " + facilityId });
         for (const [field,v] of Object.entries({"facilityName":facilityName, "xCoordinate":xCoordinate, "yCoordinate":yCoordinate})){
@@ -53,7 +53,18 @@ export async function updateFacility(req: Request, res: Response) {
                 facility[field] = v;
             }
         }
-        await facility.save();
+
+        const p1 : Promise<Facility> = facility.save();
+
+        if (facilityDetailJson !== undefined) {
+            const facilityDetail = await facility.getFacilityDetail();
+            if (facilityDetail === undefined) throw {message: "Unable to find facilityDetail for facilityId " + facilityId}
+            for (const [field,v] of Object.entries(facilityDetailJson)){
+                facilityDetail[field] = v;
+            }
+            await facilityDetail.save();
+        }
+        await p1;
         return res.status(200).json({facility:  facility.toJSON()})
     } catch (error: any) {
         res.status(400).json({ error: error.message });
