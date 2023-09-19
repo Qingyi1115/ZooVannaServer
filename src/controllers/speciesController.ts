488137e167f849961d3adf8c73c6fb0272ac9e9c
JasonDeKhanh <giakhanh.lehoang@gmail.com>
2023-09-19 18:01:57 +0800

    updateSpecies in controller works without image. Now takes in lifeExpectancyYears as well

--- src/controllers/speciesController.ts
+++ src/controllers/speciesController.ts
@@ -7,283 +7,294 @@ import * as SpeciesService from "../services/species";
 import { handleFileUpload } from "../helpers/multerProcessFile";
 
 export async function getAllSpecies(req: Request, res: Response) {
-  try {
-    const allSpecies = await SpeciesService.getAllSpecies();
-    return res.status(200).json(allSpecies);
-  } catch (error: any) {
-    res.status(400).json({ error: error.message });
-  }
+    try {
+        const allSpecies = await SpeciesService.getAllSpecies();
+        return res.status(200).json(allSpecies);
+    } catch (error: any) {
+        res.status(400).json({ error: error.message });
+    }
 }
 
 export async function getSpeciesByCode(req: Request, res: Response) {
-  const { speciesCode } = req.params;
+    const { speciesCode } = req.params;
 
-  if (speciesCode == undefined) {
-    console.log("Missing field(s): ", {
-      speciesCode,
-    });
-    return res.status(400).json({ error: "Missing information!" });
-  }
+    if (speciesCode == undefined) {
+        console.log("Missing field(s): ", {
+            speciesCode,
+        });
+        return res.status(400).json({ error: "Missing information!" });
+    }
 
-  try {
-    const species = await SpeciesService.getSpeciesByCode(speciesCode);
-    return res.status(200).json(species);
-  } catch (error: any) {
-    res.status(400).json({ error: error.message });
-  }
+    try {
+        const species = await SpeciesService.getSpeciesByCode(speciesCode);
+        return res.status(200).json(species);
+    } catch (error: any) {
+        res.status(400).json({ error: error.message });
+    }
 }
 
 export async function createSpecies(req: Request, res: Response) {
-  try {
-    // const { email } = (req as any).locals.jwtPayload
-    // const employee = await findEmployeeByEmail(email);
+    try {
+        // const { email } = (req as any).locals.jwtPayload
+        // const employee = await findEmployeeByEmail(email);
 
-    // if (!((await employee.getPlanningStaff())?.plannerType == PlannerType.OPERATIONS_MANAGER)) {
-    //     return res.status(403).json({error: "Access Denied! Operation managers only!"});
-    // }
+        // if (!((await employee.getPlanningStaff())?.plannerType == PlannerType.OPERATIONS_MANAGER)) {
+        //     return res.status(403).json({error: "Access Denied! Operation managers only!"});
+        // }
 
-    const imageUrl = await handleFileUpload(
-      req,
-      process.env.IMG_URL_ROOT! + "species", //"D:/capstoneUploads/species",
-    );
-    const {
-      commonName,
-      scientificName,
-      aliasName,
-      conservationStatus,
-      domain,
-      kingdom,
-      phylum,
-      speciesClass,
-      order,
-      family,
-      genus,
-      educationalDescription,
-      nativeContinent,
-      nativeBiomes,
-      groupSexualDynamic,
-      habitatOrExhibit,
-      generalDietPreference,
-      lifeExpectancyYears,
-    } = req.body;
+        const imageUrl = await handleFileUpload(
+            req,
+            process.env.IMG_URL_ROOT! + "species", //"D:/capstoneUploads/species",
+        );
+        const {
+            commonName,
+            scientificName,
+            aliasName,
+            conservationStatus,
+            domain,
+            kingdom,
+            phylum,
+            speciesClass,
+            order,
+            family,
+            genus,
+            educationalDescription,
+            nativeContinent,
+            nativeBiomes,
+            groupSexualDynamic,
+            habitatOrExhibit,
+            generalDietPreference,
+            lifeExpectancyYears,
+        } = req.body;
 
-    if (
-      [
-        commonName,
-        scientificName,
-        aliasName,
-        conservationStatus,
-        domain,
-        kingdom,
-        phylum,
-        speciesClass,
-        order,
-        family,
-        genus,
-        educationalDescription,
-        nativeContinent,
-        nativeBiomes,
-        groupSexualDynamic,
-        habitatOrExhibit,
-        generalDietPreference,
-        lifeExpectancyYears,
-      ].includes(undefined)
-    ) {
-      console.log("Missing field(s): ", {
-        commonName,
-        scientificName,
-        aliasName,
-        conservationStatus,
-        domain,
-        kingdom,
-        phylum,
-        speciesClass,
-        order,
-        family,
-        genus,
-        educationalDescription,
-        nativeContinent,
-        nativeBiomes,
-        groupSexualDynamic,
-        habitatOrExhibit,
-        generalDietPreference,
-        lifeExpectancyYears,
-      });
-      return res.status(400).json({ error: "Missing information!" });
+        if (
+            [
+                commonName,
+                scientificName,
+                aliasName,
+                conservationStatus,
+                domain,
+                kingdom,
+                phylum,
+                speciesClass,
+                order,
+                family,
+                genus,
+                educationalDescription,
+                nativeContinent,
+                nativeBiomes,
+                groupSexualDynamic,
+                habitatOrExhibit,
+                generalDietPreference,
+                lifeExpectancyYears,
+            ].includes(undefined)
+        ) {
+            console.log("Missing field(s): ", {
+                commonName,
+                scientificName,
+                aliasName,
+                conservationStatus,
+                domain,
+                kingdom,
+                phylum,
+                speciesClass,
+                order,
+                family,
+                genus,
+                educationalDescription,
+                nativeContinent,
+                nativeBiomes,
+                groupSexualDynamic,
+                habitatOrExhibit,
+                generalDietPreference,
+                lifeExpectancyYears,
+            });
+            return res.status(400).json({ error: "Missing information!" });
+        }
+
+        // have to pass in req for image uploading
+        let species = await SpeciesService.createNewSpecies(
+            commonName,
+            scientificName,
+            aliasName,
+            conservationStatus,
+            domain,
+            kingdom,
+            phylum,
+            speciesClass,
+            order,
+            family,
+            genus,
+            educationalDescription,
+            nativeContinent,
+            nativeBiomes,
+            groupSexualDynamic,
+            habitatOrExhibit,
+            generalDietPreference,
+            imageUrl,
+            lifeExpectancyYears,
+        );
+
+        return res.status(200).json({ species });
+    } catch (error: any) {
+        res.status(400).json({ error: error.message });
     }
-
-    // have to pass in req for image uploading
-    let species = await SpeciesService.createNewSpecies(
-      commonName,
-      scientificName,
-      aliasName,
-      conservationStatus,
-      domain,
-      kingdom,
-      phylum,
-      speciesClass,
-      order,
-      family,
-      genus,
-      educationalDescription,
-      nativeContinent,
-      nativeBiomes,
-      groupSexualDynamic,
-      habitatOrExhibit,
-      generalDietPreference,
-      imageUrl,
-      lifeExpectancyYears,
-    );
-
-    return res.status(200).json({ species });
-  } catch (error: any) {
-    res.status(400).json({ error: error.message });
-  }
 }
 
 export async function updateSpecies(req: Request, res: Response) {
-  try {
-    const imageUrl = await handleFileUpload(
-      req,
-      process.env.IMG_URL_ROOT! + "species", //"D:/capstoneUploads/species",
-    );
-    const {
-      speciesCode,
-      commonName,
-      scientificName,
-      aliasName,
-      conservationStatus,
-      domain,
-      kingdom,
-      phylum,
-      speciesClass,
-      order,
-      family,
-      genus,
-      educationalDescription,
-      nativeContinent,
-      nativeBiomes,
-      groupSexualDynamic,
-      habitatOrExhibit,
-      generalDietPreference,
-    } = req.body;
+    try {
+        let imageUrl
+        if (req.headers['content-type'] && req.headers['content-type'].includes('multipart/form-data')) {
+            imageUrl = await handleFileUpload(
+                req,
+                process.env.IMG_URL_ROOT! + "species", //"D:/capstoneUploads/species",
+            );
+        } else {
+            imageUrl = req.body.imageUrl
+        }
+        const {
+            speciesCode,
+            commonName,
+            scientificName,
+            aliasName,
+            conservationStatus,
+            domain,
+            kingdom,
+            phylum,
+            speciesClass,
+            order,
+            family,
+            genus,
+            educationalDescription,
+            nativeContinent,
+            nativeBiomes,
+            groupSexualDynamic,
+            habitatOrExhibit,
+            generalDietPreference,
+            lifeExpectancyYears
+        } = req.body;
 
-    if (
-      [
-        speciesCode,
-        commonName,
-        scientificName,
-        aliasName,
-        conservationStatus,
-        domain,
-        kingdom,
-        phylum,
-        speciesClass,
-        order,
-        family,
-        genus,
-        educationalDescription,
-        nativeContinent,
-        nativeBiomes,
-        groupSexualDynamic,
-        habitatOrExhibit,
-        generalDietPreference,
-      ].includes(undefined)
-    ) {
-      console.log("Missing field(s): ", {
-        speciesCode,
-        commonName,
-        scientificName,
-        aliasName,
-        conservationStatus,
-        domain,
-        kingdom,
-        phylum,
-        speciesClass,
-        order,
-        family,
-        genus,
-        educationalDescription,
-        nativeContinent,
-        nativeBiomes,
-        groupSexualDynamic,
-        habitatOrExhibit,
-        generalDietPreference,
-      });
-      return res.status(400).json({ error: "Missing information!" });
+        if (
+            [
+                speciesCode,
+                commonName,
+                scientificName,
+                aliasName,
+                conservationStatus,
+                domain,
+                kingdom,
+                phylum,
+                speciesClass,
+                order,
+                family,
+                genus,
+                educationalDescription,
+                nativeContinent,
+                nativeBiomes,
+                groupSexualDynamic,
+                habitatOrExhibit,
+                generalDietPreference,
+                lifeExpectancyYears,
+                imageUrl
+            ].includes(undefined)
+        ) {
+            console.log("Missing field(s): ", {
+                speciesCode,
+                commonName,
+                scientificName,
+                aliasName,
+                conservationStatus,
+                domain,
+                kingdom,
+                phylum,
+                speciesClass,
+                order,
+                family,
+                genus,
+                educationalDescription,
+                nativeContinent,
+                nativeBiomes,
+                groupSexualDynamic,
+                habitatOrExhibit,
+                generalDietPreference,
+                lifeExpectancyYears,
+                imageUrl
+            });
+            return res.status(400).json({ error: "Missing information!" });
+        }
+
+        // have to pass in req for image uploading
+        let species = await SpeciesService.updateSpecies(
+            speciesCode,
+            commonName,
+            scientificName,
+            aliasName,
+            conservationStatus,
+            domain,
+            kingdom,
+            phylum,
+            speciesClass,
+            order,
+            family,
+            genus,
+            educationalDescription,
+            nativeContinent,
+            nativeBiomes,
+            groupSexualDynamic,
+            habitatOrExhibit,
+            generalDietPreference,
+            imageUrl,
+            lifeExpectancyYears
+        );
+
+        return res.status(200).json({ species });
+    } catch (error: any) {
+        res.status(400).json({ error: error.message });
     }
-
-    // have to pass in req for image uploading
-    let species = await SpeciesService.updateSpecies(
-      speciesCode,
-      commonName,
-      scientificName,
-      aliasName,
-      conservationStatus,
-      domain,
-      kingdom,
-      phylum,
-      speciesClass,
-      order,
-      family,
-      genus,
-      educationalDescription,
-      nativeContinent,
-      nativeBiomes,
-      groupSexualDynamic,
-      habitatOrExhibit,
-      generalDietPreference,
-      imageUrl,
-    );
-
-    return res.status(200).json({ species });
-  } catch (error: any) {
-    res.status(400).json({ error: error.message });
-  }
 }
 
 export async function updateSpeciesEduDesc(req: Request, res: Response) {
-  try {
-    // const imageUrl = await handleFileUpload(
-    //     req,
-    //     process.env.IMG_URL_ROOT! + "species", //"D:/capstoneUploads/species",
-    // );
-    const { speciesCode, educationalDescription } = req.body;
+    try {
+        // const imageUrl = await handleFileUpload(
+        //     req,
+        //     process.env.IMG_URL_ROOT! + "species", //"D:/capstoneUploads/species",
+        // );
+        const { speciesCode, educationalDescription } = req.body;
 
-    if ([speciesCode, educationalDescription].includes(undefined)) {
-      console.log("Missing field(s): ", {
-        speciesCode,
-        educationalDescription,
-      });
-      return res.status(400).json({ error: "Missing information!" });
+        if ([speciesCode, educationalDescription].includes(undefined)) {
+            console.log("Missing field(s): ", {
+                speciesCode,
+                educationalDescription,
+            });
+            return res.status(400).json({ error: "Missing information!" });
+        }
+
+        // have to pass in req for image uploading
+        let species = await SpeciesService.updateSpeciesEduDesc(
+            speciesCode,
+            educationalDescription,
+        );
+
+        return res.status(200).json({ species });
+    } catch (error: any) {
+        res.status(400).json({ error: error.message });
     }
-
-    // have to pass in req for image uploading
-    let species = await SpeciesService.updateSpeciesEduDesc(
-      speciesCode,
-      educationalDescription,
-    );
-
-    return res.status(200).json({ species });
-  } catch (error: any) {
-    res.status(400).json({ error: error.message });
-  }
 }
 
 export async function deleteSpeciesByCode(req: Request, res: Response) {
-  const { speciesCode } = req.params;
+    const { speciesCode } = req.params;
 
-  if (speciesCode == undefined) {
-    console.log("Missing field(s): ", {
-      speciesCode,
-    });
-    return res.status(400).json({ error: "Missing information!" });
-  }
+    if (speciesCode == undefined) {
+        console.log("Missing field(s): ", {
+            speciesCode,
+        });
+        return res.status(400).json({ error: "Missing information!" });
+    }
 
-  try {
-    const species = await SpeciesService.deleteSpeciesByCode(speciesCode);
-    return res.status(200).json(species);
-  } catch (error: any) {
-    res.status(400).json({ error: error.message });
-  }
+    try {
+        const species = await SpeciesService.deleteSpeciesByCode(speciesCode);
+        return res.status(200).json(species);
+    } catch (error: any) {
+        res.status(400).json({ error: error.message });
+    }
 }
--- src/services/species.ts
+++ src/services/species.ts
@@ -3,159 +3,161 @@ import { validationErrorHandler } from "../helpers/errorHandler";
 import { Species } from "../models/species";
 
 export async function getAllSpecies() {
-  try {
-    const allSpecies = await Species.findAll();
-    return allSpecies;
-  } catch (error: any) {
-    throw validationErrorHandler(error);
-  }
+    try {
+        const allSpecies = await Species.findAll();
+        return allSpecies;
+    } catch (error: any) {
+        throw validationErrorHandler(error);
+    }
 }
 
 export async function getSpeciesByCode(speciesCode: string) {
-  let result = await Species.findOne({
-    where: { speciesCode: speciesCode },
-  });
-  if (result) {
-    return result;
-  }
-  throw { error: "Invalid Species Code!" };
+    let result = await Species.findOne({
+        where: { speciesCode: speciesCode },
+    });
+    if (result) {
+        return result;
+    }
+    throw { error: "Invalid Species Code!" };
 }
 
 export async function createNewSpecies(
-  commonName: string,
-  scientificName: string,
-  aliasName: string,
-  conservationStatus: string,
-  domain: string,
-  kingdom: string,
-  phylum: string,
-  speciesClass: string,
-  order: string,
-  family: string,
-  genus: string,
-  educationalDescription: string,
-  nativeContinent: string,
-  nativeBiomes: string,
-  groupSexualDynamic: string,
-  habitatOrExhibit: string,
-  generalDietPreference: string,
-  imageUrl: string,
-  lifeExpectancyYears: number,
-  // foodRemark: string,
+    commonName: string,
+    scientificName: string,
+    aliasName: string,
+    conservationStatus: string,
+    domain: string,
+    kingdom: string,
+    phylum: string,
+    speciesClass: string,
+    order: string,
+    family: string,
+    genus: string,
+    educationalDescription: string,
+    nativeContinent: string,
+    nativeBiomes: string,
+    groupSexualDynamic: string,
+    habitatOrExhibit: string,
+    generalDietPreference: string,
+    imageUrl: string,
+    lifeExpectancyYears: number,
+    // foodRemark: string,
 ) {
-  let newSpecies = {
-    speciesCode: await Species.getNextSpeciesCode(),
-    commonName: commonName,
-    scientificName: scientificName,
-    aliasName: aliasName,
-    conservationStatus: conservationStatus,
-    domain: domain,
-    kingdom: kingdom,
-    phylum: phylum,
-    speciesClass: speciesClass,
-    order: order,
-    family: family,
-    genus: genus,
-    educationalDescription: educationalDescription,
-    nativeContinent: nativeContinent,
-    nativeBiomes: nativeBiomes,
-    groupSexualDynamic: groupSexualDynamic,
-    habitatOrExhibit: habitatOrExhibit,
-    generalDietPreference: generalDietPreference,
-    imageUrl: imageUrl,
-    lifeExpectancyYears,
-    // foodRemark
-  } as any;
+    let newSpecies = {
+        speciesCode: await Species.getNextSpeciesCode(),
+        commonName: commonName,
+        scientificName: scientificName,
+        aliasName: aliasName,
+        conservationStatus: conservationStatus,
+        domain: domain,
+        kingdom: kingdom,
+        phylum: phylum,
+        speciesClass: speciesClass,
+        order: order,
+        family: family,
+        genus: genus,
+        educationalDescription: educationalDescription,
+        nativeContinent: nativeContinent,
+        nativeBiomes: nativeBiomes,
+        groupSexualDynamic: groupSexualDynamic,
+        habitatOrExhibit: habitatOrExhibit,
+        generalDietPreference: generalDietPreference,
+        imageUrl: imageUrl,
+        lifeExpectancyYears,
+        // foodRemark
+    } as any;
 
-  console.log(newSpecies);
+    console.log(newSpecies);
 
-  try {
-    return Species.create(newSpecies);
-  } catch (error: any) {
-    console.log(error);
-    throw validationErrorHandler(error);
-  }
+    try {
+        return Species.create(newSpecies);
+    } catch (error: any) {
+        console.log(error);
+        throw validationErrorHandler(error);
+    }
 }
 
 export async function updateSpecies(
-  speciesCode: string,
-  commonName: string,
-  scientificName: string,
-  aliasName: string,
-  conservationStatus: string,
-  domain: string,
-  kingdom: string,
-  phylum: string,
-  speciesClass: string,
-  order: string,
-  family: string,
-  genus: string,
-  educationalDescription: string,
-  nativeContinent: string,
-  selectedBiomes: string,
-  groupSexualDynamic: string,
-  habitatOrExhibit: string,
-  generalDietPreference: string,
-  imageUrl: string,
+    speciesCode: string,
+    commonName: string,
+    scientificName: string,
+    aliasName: string,
+    conservationStatus: string,
+    domain: string,
+    kingdom: string,
+    phylum: string,
+    speciesClass: string,
+    order: string,
+    family: string,
+    genus: string,
+    educationalDescription: string,
+    nativeContinent: string,
+    selectedBiomes: string,
+    groupSexualDynamic: string,
+    habitatOrExhibit: string,
+    generalDietPreference: string,
+    imageUrl: string,
+    lifeExpectancyYears: number,
 ) {
-  let updatedSpecies = {
-    speciesCode: speciesCode,
-    commonName: commonName,
-    scientificName: scientificName,
-    aliasName: aliasName,
-    conservationStatus: conservationStatus,
-    domain: domain,
-    kingdom: kingdom,
-    phylum: phylum,
-    speciesClass: speciesClass,
-    order: order,
-    family: family,
-    genus: genus,
-    educationalDescription: educationalDescription,
-    nativeContinent: nativeContinent,
-    nativeBiomes: selectedBiomes,
-    groupSexualDynamic: groupSexualDynamic,
-    habitatOrExhibit: habitatOrExhibit,
-    generalDietPreference: generalDietPreference,
-    imageUrl: imageUrl,
-  } as any;
+    let updatedSpecies = {
+        speciesCode: speciesCode,
+        commonName: commonName,
+        scientificName: scientificName,
+        aliasName: aliasName,
+        conservationStatus: conservationStatus,
+        domain: domain,
+        kingdom: kingdom,
+        phylum: phylum,
+        speciesClass: speciesClass,
+        order: order,
+        family: family,
+        genus: genus,
+        educationalDescription: educationalDescription,
+        nativeContinent: nativeContinent,
+        nativeBiomes: selectedBiomes,
+        groupSexualDynamic: groupSexualDynamic,
+        habitatOrExhibit: habitatOrExhibit,
+        generalDietPreference: generalDietPreference,
+        imageUrl: imageUrl,
+        lifeExpectancyYears: lifeExpectancyYears,
+    } as any;
 
-  console.log(updatedSpecies);
+    console.log(updatedSpecies);
 
-  try {
-    let species = await Species.update(updatedSpecies, {
-      where: { speciesCode: speciesCode },
-    });
-  } catch (error: any) {
-    throw validationErrorHandler(error);
-  }
+    try {
+        let species = await Species.update(updatedSpecies, {
+            where: { speciesCode: speciesCode },
+        });
+    } catch (error: any) {
+        throw validationErrorHandler(error);
+    }
 }
 
 export async function updateSpeciesEduDesc(
-  speciesCode: string,
-  educationalDescription: string,
+    speciesCode: string,
+    educationalDescription: string,
 ) {
-  let updatedSpecies = {
-    educationalDescription: educationalDescription,
-  } as any;
+    let updatedSpecies = {
+        educationalDescription: educationalDescription,
+    } as any;
 
-  console.log(updatedSpecies);
+    console.log(updatedSpecies);
 
-  try {
-    let species = await Species.update(updatedSpecies, {
-      where: { speciesCode: speciesCode },
-    });
-  } catch (error: any) {
-    throw validationErrorHandler(error);
-  }
+    try {
+        let species = await Species.update(updatedSpecies, {
+            where: { speciesCode: speciesCode },
+        });
+    } catch (error: any) {
+        throw validationErrorHandler(error);
+    }
 }
 
 export async function deleteSpeciesByCode(speciesCode: string) {
-  let result = await Species.destroy({
-    where: { speciesCode: speciesCode },
-  });
-  if (result) {
-    return result;
-  }
-  throw { error: "Invalid Species Code!" };
+    let result = await Species.destroy({
+        where: { speciesCode: speciesCode },
+    });
+    if (result) {
+        return result;
+    }
+    throw { error: "Invalid Species Code!" };
 }
