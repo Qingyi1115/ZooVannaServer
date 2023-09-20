251335ec529a9fd663d6ba4462ebd3f11d5b4be8
Owner <e0540022@u.nus.edu>
2023-09-20 20:31:38 +0800

    Update predictors.ts

--- src/helpers/predictors.ts
+++ src/helpers/predictors.ts
@@ -10,14 +10,7 @@ export function predictNextDate(dates:Date[]):Date|void{
         )
     }
     if (dates.length>10){
-        // console.log("123", dates)
-        // console.log("223")
-        // console.log(intervals)
         const results = (getAugumentedDataset(intervals, 1)as any) ["augumentedDataset"];
-        // console.log("333")
-        // console.log(results)
-        // console.log(results[results.length - 1])
-        // console.log(dates[0].getTime())
         return new Date(dates[0].getTime() + results[results.length - 1] * 1000 * 60 * 60* 24);
     }else{
         let average = 0
@@ -27,16 +20,3 @@ export function predictNextDate(dates:Date[]):Date|void{
         return new Date(dates[0].getTime() + average * 1000 * 60 * 60* 24);
     }
 }
-
-
-// OUTPUT:
-// {
-// augumentedDataset: [1, 4, 5, 3, 1, 3, 4, 1, 2, 4, 5, 5, 1.3338017069503336, 2.9451723210706824, 5.691675635182751, 6.721827583201698],
-// alpha: 0.1,
-// beta: 1,
-// gamma: 0.4,
-// period: 5,
-// mse: 2.0945071482039226,
-// sse: 14.661550037427457,
-// mpe: 0.06217855085131068
-// }
