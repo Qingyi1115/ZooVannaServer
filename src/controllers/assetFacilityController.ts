1145b7c9f01ea6c5cf3eab80c0c628242f6d8382
Ming Jun <77621187+E0540022@users.noreply.github.com>
2023-10-20 18:14:49 +0800

    Update assetFacilityController.ts

--- src/controllers/assetFacilityController.ts
+++ src/controllers/assetFacilityController.ts
@@ -862,13 +862,13 @@ export async function createFacilityMaintenanceLogController(
       remarks,
       employee.employeeName
     );
-    const generalStaff = await employee.getGeneralStaff();
-    const facilities = await generalStaff?.getMaintainedFacilities() || [];
-    for (const facility of facilities) {
-      if ((await facility.getFacility()).facilityId == Number(facilityId))
-        generalStaff.removeMaintainedFacility(facility);
-    }
-    await generalStaff?.save();
+    // const generalStaff = await employee.getGeneralStaff();
+    // const facilities = await generalStaff?.getMaintainedFacilities() || [];
+    // for (const facility of facilities) {
+    //   if ((await facility.getFacility()).facilityId == Number(facilityId))
+    //     generalStaff.removeMaintainedFacility(facility);
+    // }
+    // await generalStaff?.save();
 
     return res.status(200).json({ maintenanceLog: maintenanceLog.toJSON() });
   } catch (error: any) {
@@ -1367,15 +1367,15 @@ export async function createSensorMaintenanceLogController(
       remarks,
       employee.employeeName
     );
-    const generalStaff = await employee?.getGeneralStaff();
-    if (generalStaff) {
-      let sensors = (await generalStaff?.getSensors()) || [];
-      for (const sensor of sensors) {
-        if (sensor.sensorId == Number(sensorId))
-          generalStaff.removeSensor(sensor);
-      }
-      await generalStaff?.save();
-    }
+    // const generalStaff = await employee?.getGeneralStaff();
+    // if (generalStaff) {
+    //   let sensors = (await generalStaff?.getSensors()) || [];
+    //   for (const sensor of sensors) {
+    //     if (sensor.sensorId == Number(sensorId))
+    //       generalStaff.removeSensor(sensor);
+    //   }
+    //   await generalStaff?.save();
+    // }
 
     return res.status(200).json({ sensor: sensor.toFullJSON() });
   } catch (error: any) {
