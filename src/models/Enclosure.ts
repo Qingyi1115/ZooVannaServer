2b0e4639f03a7179ae06871673391b6ec37219f8
Qingyi <xqy1115@gmail.com>
2023-11-13 15:37:28 +0800

    i did nothing

--- src/models/Enclosure.ts
+++ src/models/Enclosure.ts
@@ -13,7 +13,7 @@ import {
   HasOneSetAssociationMixin,
   InferAttributes,
   InferCreationAttributes,
-  Model
+  Model,
 } from "Sequelize";
 import { conn } from "../db";
 import { Animal } from "./Animal";
@@ -220,4 +220,3 @@ Enclosure.init(
 );
 
 export { Enclosure };
-
